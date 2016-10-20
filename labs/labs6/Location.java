95550df021db3b65f63da84acd5bd08acdd34fde
Alexa Javellana <AlexaJ@148-100-135-137.FoxNet.marist.edu>
2016-10-20 13:32:13 -0400

    prob 913 and its location added lab6

---
+++ labs/labs6/Location.java
@@ -0,0 +1,13 @@
+public class Location {
+
+  public int row;
+  public int column;
+  public double maxValue;
+
+  public Location() {
+    row = 1;
+    column = 1;
+  }
+    
+
+}
---
+++ labs/labs6/Problem913.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Problem913 {
+  public static void main(String[] args) {
+  
+  Scanner input =  new Scanner(System.in);
+  System.out.print("Enter the number of rows and columns in the array: ");  
+  int noRows = input.nextInt();
+  int noCols = input.nextInt();    
+  
+  double[][] newArray = new double[noRows][noCols];
+  
+  }
+ } 
