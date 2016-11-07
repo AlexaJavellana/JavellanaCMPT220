2dac9f5b0704fd2e17751ebdff8f863cd9d9330a
Alexa Javellana <AlexaJ@148-100-135-137.FoxNet.marist.edu>
2016-11-07 16:55:21 -0500

    lab8 added

---
+++ labs/lab8/Problem1113.java
@@ -0,0 +1,40 @@
+/*
+ * file: Problem1113.java
+ * author: Alexa Javellana
+ * course: CMPT 220
+ * assignment: lab 8
+ * due date: November 4th 2016
+ * version: 1.0
+ *
+ * This file contains problem Problem 11.13
+ */
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Problem1113 {
+	public static void main(String[] args) {
+
+ 	System.out.print("Enter ten integers: ");
+ 	Scanner input = new Scanner(System.in);
+ 	ArrayList<Integer> list = new ArrayList<>();
+ 		for (int i = 0; i < 10; i++)
+ 			list.add(input.nextInt());
+  
+  System.out.print("The distinct intergers are: ");
+  removeDuplicate(list);
+  System.out.println(list);
+	}
+
+	public static void removeDuplicate(ArrayList<Integer> list) {
+
+		ArrayList<Integer> temp = new ArrayList<>();
+		for (int i = 0; i < list.size(); i++) {
+			if (!temp.contains(list.get(i))) {
+				temp.add(list.get(i));
+			}
+		}	
+			list.clear();
+			list.addAll(temp);
+	}
+}
