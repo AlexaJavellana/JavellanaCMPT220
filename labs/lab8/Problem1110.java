ae8482ed7838d9b0a323ff89a8c9d7584da90064
Alexa Javellana <AlexaJ@148-100-135-137.FoxNet.marist.edu>
2016-11-07 19:04:12 -0500

    lab8

---
+++ labs/lab8/Problem1110.java
@@ -0,0 +1,18 @@
+/*
+ * file: Problem1110.java
+ * author: Alexa Javellana
+ * course: CMPT 220
+ * assignment: lab 8
+ * due date: November 4th 2016
+ * version: 1.0
+ *
+ * This file contains problem Problem 11.10
+ (Implement MyStack using inheritance) In Listing 11.10,
+  MyStack is imple- mented using composition. Define a new 
+  stack class that extends ArrayList.
+Draw the UML diagram for the classes and then implement
+ MyStack. Write a test program that prompts the user to enter 
+ five strings and displays them in reverse order.
+ */
+
+import java.util.ArrayList;
