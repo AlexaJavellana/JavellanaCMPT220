f3a95a16fe3c8e32002a485e8f9ccc4806284947
Alexa Javellana <AlexaJ@148-100-134-247.FoxNet.marist.edu>
2016-12-09 18:18:09 -0500

    project 2 fully uploaded, including code and writeup

--- projects/project2/code/RecipeRun.java
+++ projects/project2/code/RecipeRun.java
@@ -6,13 +6,18 @@ public class RecipeRun {
 	Scanner input = new Scanner(System.in); 
 	System.out.println("Welcome to Recipe Run, a college-friendly " + 
 										"random recipe generator. Please enter your " +
-										"budget under 20.01 USD: ");
+										"budget more than 0 but under 20 USD: ");
   double userMoney = input.nextDouble();
 
   if (userMoney >= 20.01) {
   	System.out.println("Invalid budget! Try again.");
   	System.exit(0);
   }	
+
+  if (userMoney <= 0) {
+    System.out.println("Invalid budget! Try again.");
+    System.exit(0);
+  } 
 
   Recipe userRecipe = new Recipe(userMoney);
  
@@ -31,7 +36,5 @@ public class RecipeRun {
   	System.out.println("Here is your budget-appropriate recipe: ");
   	userRecipe.showNonVRecipe();
 	}
-
-// draft 1 milestone 
 	}
 }
